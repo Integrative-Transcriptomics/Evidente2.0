cfa9c5ee7b6f2a1c80e9ed41fb84f564b9ae4ae8
Sophie Pesch <sophie.pesch@web.de>
2021-07-30 15:59:38 +0200

    added button component

---
+++ src/components/my-button.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import Button from "react-bootstrap/Button";
+
+class MyButton extends Component{
+       
+        render(){
+            return(
+            <Button id= "go-terms" variant= "light" onClick={this.props.onClick} style={{float:'right'}}>
+                    {this.props.text}
+            </Button>
+            )
+        };
+    
+}
+export default MyButton
